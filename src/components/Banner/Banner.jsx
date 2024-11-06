e790faefc7ebc8d555b3dfdbd2b0da5203547938
Nahar0912 <anika.viki987@gmail.com>
2024-11-06 20:34:26 +0600

    next2

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,8 @@
 const Banner = () => {
     return (
-        <div className="mx-10 flex flex-col justify-center items-center">
+        <div className="mx-10 -mt-20 flex flex-col justify-center items-center">
             <div className="bg-purple-600 pb-48 rounded-3xl h-full p-14 text-white text-center flex flex-col ">
-                <div className="px-56">
+                <div className="px-56 pt-5">
                     <h1 className="text-4xl font-bold mt-4">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h1><br />
                     <p>Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all! </p><br />
                     <button className="btn rounded-full px-10 text-purple-700">Shop Now</button>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,61 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+    const location = useLocation();
+    const isHomePage = location.pathname === "/";
 
-    const links = <>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/statistics">Statistics</NavLink></li>          
-        <li><NavLink to="/dashboard">Dashboard</NavLink></li>
-    </>
+    const links = (
+        <>
+            <li>
+                <NavLink to="/" className={isHomePage ? "text-white" : ""}>
+                    Home
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/statistics" className={isHomePage ? "text-white" : ""}>
+                    Statistics
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/dashboard" className={isHomePage ? "text-white" : ""}>
+                    Dashboard
+                </NavLink>
+            </li>
+        </>
+    );
 
     return (
-        <div className="navbar px-10 bg-inherit">
+        <div className={`navbar px-14 my-3 bg-inherit z-20 relative w-full ${isHomePage ? "text-white" : "text-black"}`}>
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4 6h16M4 12h8m-8 6h16" />
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16"
+                            />
                         </svg>
                     </div>
                     <ul
                         tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                    >
                         <li><a>Item 1</a></li>
-                        <li>
-                        <a>Parent</a>
-                        </li>
+                        <li><a>Parent</a></li>
                         <li><a>Item 3</a></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl font-bold">Gadget Heaven</a>
+                <a className={`btn btn-ghost text-xl font-bold ${isHomePage ? "text-white" : ""}`}>
+                    Gadget Heaven
+                </a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
@@ -52,19 +71,22 @@ const Navbar = () => {
                                 className="h-5 w-5"
                                 fill="none"
                                 viewBox="0 0 24 24"
-                                stroke="currentColor">
+                                stroke="currentColor"
+                            >
                                 <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                                />
                             </svg>
                             <span className="badge badge-sm indicator-item">8</span>
                         </div>
                     </div>
                     <div
                         tabIndex={0}
-                        className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
+                        className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
+                    >
                         <div className="card-body">
                             <span className="text-lg font-bold">8 Items</span>
                             <span className="text-info">Subtotal: $999</span>
@@ -75,13 +97,24 @@ const Navbar = () => {
                     </div>
                 </div>
                 <button className="btn btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                        />
                     </svg>
                 </button>
             </div>
         </div>
-     );
+    );
 };
 
 export default Navbar;
